72deceaea8fe16d2e6c2f66aa230935a61313a57
Tanmay Chordia <tchordia@gmail.com>
2014-03-29 21:39:02 -0700

    Added new Project (For reals this time loll)

---
+++ BottleRocketSim(Tanmay)/src/src/GUIpack/LaunchPanel.java
@@ -0,0 +1,171 @@
+/**
+ * 
+ */
+package src.GUIpack;
+
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+
+/**
+ * @author Tanmay
+ *
+ */
+public class LaunchPanel extends JPanel implements ActionListener {
+
+	/**
+	 * 
+	 */
+	
+	public SliderGui sliderp = new SliderGui();
+	
+	public GUI gui = new GUI();
+	public ImageIcon icon = createImageIcon("background.jpeg");
+//	public LaunchPanel() {
+//		// TODO Auto-generated constructor stub
+//		setLayout(new GridLayout());
+//		setVisible(true);
+//	    add(this.spanel);
+//	    add(this.apanel);
+//	    Border bord = BorderFactory.createLineBorder(Color.black, 5);
+//		setBorder(bord); 
+//	    setVisible(true);
+//	}
+	public LaunchPanel()
+	{
+		setLayout(new GridLayout());
+		
+		
+		gui.setLayout(new GridLayout());
+	
+		add(sliderp);
+	}
+
+	/**
+	 * @param layout
+	 */
+	public LaunchPanel(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+	protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = LaunchPanel.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+	/**
+	 * @param isDoubleBuffered
+	 */
+	public LaunchPanel(boolean isDoubleBuffered) {
+		super(isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param layout
+	 * @param isDoubleBuffered
+	 */
+	public LaunchPanel(LayoutManager layout, boolean isDoubleBuffered) {
+		super(layout, isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	public class SliderGui extends JPanel
+	{
+		public SPanel spanel = new SPanel();
+		public GraphPanel apanel = new GraphPanel(new RocketMath2());
+		public BouncingBall ball = new BouncingBall();
+		public SliderGui()
+		{
+			setLayout(new GridLayout());
+			add(spanel);
+			add(apanel);
+			add(ball);
+		}
+		public class SPanel extends SliderPanel2 implements ActionListener
+		   {
+			boolean a = true;   
+			 JButton submit = new JButton("Animate Slider Values");
+			public SPanel() 
+			   {
+				   super();
+				  
+				   submit.addActionListener(this);
+				   add(submit);
+				   repaint();
+			   }
+			   RocketMath2 rocket2;
+			   @Override
+			   public void actionPerformed(ActionEvent arg0)
+			   {
+				   System.out.println("massRocket: " + (massRocket.getValue()/(10+0.0))) ;
+					System.out.println("massWater: " + massWater.getValue()/(10+0.0)) ;
+					System.out.println("volumeBottle: " + volumeBottle.getValue()/(10000+0.0)) ;
+					System.out.println("airPressure: " + airPressure.getValue()) ;
+					System.out.println("dragC: " + dragC.getValue()) ;
+					System.out.println("bottleRadius: " + bottleRadius.getValue()/(100+0.0)) ;
+					System.out.println("nozzleRadius: " + nozzleRadius.getValue()/(100+0.0)) ;
+					rocket2 = new RocketMath2(massRocket.getValue()/(10 + 0.0), massWater.getValue()/(10 + 0.0), volumeBottle.getValue()/(10000 + 0.0),airPressure.getValue() * 10000,dragC.getValue(),bottleRadius.getValue()/(100 + 0.0), nozzleRadius.getValue()/(100 + 0.0));
+					
+					RocketMath2 r = rocket2.copy();
+					apanel.updaterr(r);
+					ball.update(rocket2);
+					
+					
+					
+			   }
+			   public void stateChanged(ChangeEvent evt)
+			   {
+				  
+				 try{
+				   JTabbedPane p = ((JTabbedPane)evt.getSource());
+				   if (a)
+				   {
+					   remove(submit);
+					   repaint();
+					   a = !a;
+				   }
+				   else
+				   {
+					   add(submit);		
+					   repaint();
+					   a = !a;
+					   }
+				 }
+				 catch( java.lang.ClassCastException e)
+				 {
+				  
+				   rocket2 = new RocketMath2(massRocket.getValue()/(10 + 0.0), massWater.getValue()/(10 + 0.0), volumeBottle.getValue()/(10000 + 0.0),airPressure.getValue() * 10000,dragC.getValue(),bottleRadius.getValue()/(100 + 0.0), nozzleRadius.getValue()/(100 + 0.0));
+				   apanel.updaterr(rocket2);
+				 }
+			   }
+			   
+			
+			   
+		   }
+	}
+	   
+	  
+
+}
---
+++ BottleRocketSim(Tanmay)/src/src/GUIpack/RocketF.java
@@ -0,0 +1,143 @@
+package src.GUIpack;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class RocketF extends JFrame  {
+	
+	
+	public GraphPanel apanel = new GraphPanel(new RocketMath2());
+    public LaunchPanel mpanel = new LaunchPanel();
+    public WPanel wpanel = new WPanel();
+	public CPanel cpanel = new CPanel();
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public RocketF ()
+    {
+		super(); 
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+        setTitle("Water Bottle Rocket Launch");
+        setSize(2000, 1000);
+        
+       
+        
+        
+        
+        
+        
+        
+        
+        
+       
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        
+        add(wpanel);
+		
+    
+    }
+
+  public void mInit()
+  {
+	  remove(wpanel);
+	  add(cpanel,BorderLayout.NORTH);
+      add(mpanel);
+      repaint();
+  }
+  
+  public void restart()
+  {
+	  remove(mpanel);
+	  remove(cpanel);
+	 
+	  add(wpanel);
+	  repaint();
+  }
+       
+   public static void main(String[] args) 
+   {
+		
+	   RocketF frame = new RocketF();
+
+    }
+
+   
+
+
+   
+   public class WPanel extends WelcomePanel
+   {
+	   public WPanel() 
+	   {
+		   super();
+		   setSize(getWidth(),getHeight());
+	   }
+	   
+	   @Override
+	   public void actionPerformed(ActionEvent e) {
+			super.actionPerformed(e);
+			System.out.println(e.getActionCommand());
+			if (e.getActionCommand().equals("launch"))
+			{
+				System.out.println(e.getActionCommand());
+				
+				mInit();
+				
+			}
+			else 
+			{
+				
+			}
+			
+		}
+	   
+	
+	   
+   }
+   
+   public class CPanel extends ControlPanel
+   {
+	   public CPanel() 
+	   {
+		   super();
+		   setSize(getWidth(),getHeight()/5);
+	   }
+	   
+	   @Override
+	   public void actionPerformed(ActionEvent e) {
+			restart();
+			//System.out.println("restart");
+//			try {
+//				Thread.sleep(1000);
+//			} catch (InterruptedException e1) {
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
+			
+			
+		}
+	   
+	
+	   
+   }
+   
+   
+   
+
+}
