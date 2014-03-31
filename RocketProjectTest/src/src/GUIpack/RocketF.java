81e904922c5bbbe21179054f3b684e8c7c0fc11c
Cameron-Yang <cameronyang1@gmail.com>
2014-03-31 09:53:09 -0700

    hopefully this will work

--- RocketProjectTest/src/src/GUIpack/RocketF.java
+++ RocketProjectTest/src/src/GUIpack/RocketF.java
@@ -1,60 +1,59 @@
 package src.GUIpack;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.io.IOException;
 
-import javax.swing.JButton;
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.event.ChangeEvent;
 
 public class RocketF extends JFrame  {
 	
 	public SPanel spanel = new SPanel();
 	public AnimPanel apanel = new AnimPanel(new RocketMath2());
-    
-
-	
-
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+
 	public RocketF ()
     {
+
 		super(); 
-		try {
-		   	    UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
-		   	} catch (Exception e) {
-		   	    e.printStackTrace();
-		   	}
+
+        for (UIManager.LookAndFeelInfo laf : UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(laf.getName())){
+                try {
+                    UIManager.setLookAndFeel(laf.getClassName());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         setTitle("Button Panel Example");
-        setSize(600, 600);
-        setLayout(new GridLayout());
-        
-        add(this.spanel);
-        add(this.apanel);
-        add(new JButton());
+        setSize(new Dimension(600, 600));
+        setLayout(new GridLayout(0,2));
+        spanel.setBackground(Color.white);
+        add(spanel);
+        add(apanel);
+        setBackground(Color.white);
+        setPreferredSize(new Dimension(1000,1000));
         apanel.setVisible(true);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-		
-    
+
     }
 
-       
-        
-       
+
    public static void main(String[] args) 
    {
-		
 	   RocketF frame = new RocketF();
+   }
 
-    }
-
-   
 
    public class SPanel extends SliderPanel
    {
@@ -70,17 +69,12 @@ public class RocketF extends JFrame  {
 		   System.out.println("massRocket: " + (massRocket.getValue()/(10+0.0))) ;
 			System.out.println("massWater: " + massWater.getValue()/(10+0.0)) ;
 			System.out.println("volumeBottle: " + volumeBottle.getValue()/(10000+0.0)) ;
-			System.out.println("airPressure: " + airPressure.getValue()) ;
+			System.out.println("airPressure: " + airPressure.getValue()*1000) ;
 			System.out.println("dragC: " + dragC.getValue()) ;
 			System.out.println("bottleRadius: " + bottleRadius.getValue()/(100+0.0)) ;
 			System.out.println("nozzleRadius: " + nozzleRadius.getValue()/(100+0.0)) ;
-			rocket2 = new RocketMath2(massRocket.getValue()/(10 + 0.0), massWater.getValue()/(10 + 0.0), volumeBottle.getValue()/(10000 + 0.0),airPressure.getValue(),dragC.getValue(),bottleRadius.getValue()/(100 + 0.0), nozzleRadius.getValue()/(100 + 0.0));
+			rocket2 = new RocketMath2(massRocket.getValue()/(10 + 0.0), massWater.getValue()/(10 + 0.0), volumeBottle.getValue()/(10000 + 0.0),airPressure.getValue()*10000,dragC.getValue(),bottleRadius.getValue()/(100 + 0.0), nozzleRadius.getValue()/(100 + 0.0));
 			apanel.updaterr(rocket2);
-			
 	   }
-	   
-	
-	   
    }
-
 }
--- RocketProjectTest/src/src/GUIpack/SliderPanel.java
+++ RocketProjectTest/src/src/GUIpack/SliderPanel.java
@@ -1,208 +1,242 @@
 package src.GUIpack;
 
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.GridLayout;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.BoxLayout;
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.JTabbedPane;
+import javax.swing.UIDefaults;
 import javax.swing.UIManager;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.JComponent;
+
+import com.sun.java.swing.Painter;
+
+/**
+ * 
+ * @author Cameron Yang, Tanmay Chordia
+ * 
+ * This class creates the Panel that contains the sliders to be used for the simulation.
+ *
+ */
 
 public class SliderPanel extends JPanel implements ChangeListener {
 
 	FlowLayout experimentLayout = new FlowLayout();
 
-	public static JSlider massRocket;
-	public static JSlider massWater;
-	public static JSlider volumeBottle;
-	public static JSlider airPressure;
-	public static JSlider dragC;
-	public static JSlider bottleRadius;
-	public static JSlider nozzleRadius;
+	public static minimalSlider massRocket;  
+	public static minimalSlider massWater;
+	public static minimalSlider volumeBottle;
+	public static minimalSlider airPressure;
+	public static minimalSlider dragC;
+	public static minimalSlider bottleRadius;
+	public static minimalSlider nozzleRadius;
+	
+    UIDefaults sliderDefaults = new UIDefaults();
+    
+    private BufferedImage sliderThumb;
 
 	public SliderPanel() {
-		try {
-			UIManager
-					.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 
-		JPanel group1 = initBack(new JPanel());
-		GridLayout flow = new GridLayout();
-		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
-		massRocket = new JSlider(JSlider.HORIZONTAL, 0, 10, 7); // divide by 100
+        for (UIManager.LookAndFeelInfo laf : UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(laf.getName())){
+                try {
+                    UIManager.setLookAndFeel(laf.getClassName());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+		JPanel sliders = new JPanel();
+		
+		JTabbedPane tabbedPane = new JTabbedPane();
+		
+		massRocket = new minimalSlider (minimalSlider.HORIZONTAL, 0, 10, 7); // divide by 100
 
 		massRocket.setMajorTickSpacing(1);
 
 		massRocket.setMinorTickSpacing(1);
 
-		massRocket.setPaintTicks(true);
-
 		massRocket.setPaintLabels(true);
-
-		massRocket.setSnapToTicks(true);
-
+		
 		massRocket.addChangeListener(this);
-		add(new JLabel("Mass of the Rocket", JLabel.CENTER));
-		group1.add(massRocket);
+		
+		JPanel rocketMassPanel = new JPanel();
+		rocketMassPanel.setBackground(Color.white);
+		rocketMassPanel.add(massRocket);
+		rocketMassPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), "Rocket Mass"));
+		
 
-		massWater = new JSlider(JSlider.HORIZONTAL, 0, 10, 6);
+		massWater = new minimalSlider(minimalSlider.HORIZONTAL, 0, 10, 6);
 
 		massWater.setMajorTickSpacing(1);
 
 		massWater.setMinorTickSpacing(1);
 
-		massWater.setPaintTicks(true);
-
 		massWater.setPaintLabels(true);
 
-		massWater.setSnapToTicks(true);
-
 		massWater.addChangeListener(this);
-		
-		group1.add(massWater);
-		volumeBottle = new JSlider(JSlider.HORIZONTAL, 0, 100, 20); // divide
-																	// value by
-																	// 10000
+
+		JPanel waterMassPanel = new JPanel();
+		waterMassPanel.setBackground(Color.white);
+		waterMassPanel.add(massWater);
+		waterMassPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), "Water Mass"));
+
+
+		volumeBottle = new minimalSlider(minimalSlider.HORIZONTAL, 0, 100, 20); // divide value by 1000
 
 		volumeBottle.setMajorTickSpacing(10);
 
-		volumeBottle.setMinorTickSpacing(1);
-
-		volumeBottle.setPaintTicks(true);
-
 		volumeBottle.setPaintLabels(true);
 
-		volumeBottle.setSnapToTicks(true);
 		volumeBottle.addChangeListener(this);
-		group1.add(volumeBottle);
-		airPressure = new JSlider(JSlider.HORIZONTAL, 200000, 1000000, 260000);
 
-		airPressure.setMajorTickSpacing(100000);
+		JPanel bottleVolumePanel = new JPanel();
+		bottleVolumePanel.setBackground(Color.white);
+		bottleVolumePanel.add(volumeBottle);
+		bottleVolumePanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), "Bottle Volume"));
 
-		airPressure.setMinorTickSpacing(10000);
 
-		airPressure.setPaintTicks(true);
+		airPressure = new minimalSlider(minimalSlider.HORIZONTAL, 20, 100, 26);
+
+		airPressure.setMajorTickSpacing(10);
+
+		airPressure.setMinorTickSpacing(1);
 
 		airPressure.setPaintLabels(true);
 
-		airPressure.setSnapToTicks(true);
 		airPressure.addChangeListener(this);
-		group1.add(airPressure);
-		dragC = new JSlider(JSlider.HORIZONTAL, 0, 5, 1);
+
+		JPanel airPressurePanel = new JPanel();
+		airPressurePanel.setBackground(Color.white);
+		airPressurePanel.add(airPressure);
+		airPressurePanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), "Air Pressure in ten-thousands"));
+
+
+		dragC = new minimalSlider(minimalSlider.HORIZONTAL, 0, 5, 1);
 
 		dragC.setMajorTickSpacing(1);
 
 		dragC.setMinorTickSpacing(1);
 
-		dragC.setPaintTicks(true);
-
 		dragC.setPaintLabels(true);
 
-		dragC.setSnapToTicks(true);
 		dragC.addChangeListener(this);
-		group1.add(dragC);
-		bottleRadius = new JSlider(JSlider.HORIZONTAL, 0, 10, 5);// divide by
-																	// 100
 
+		JPanel dragCoefficientPanel = new JPanel();
+
+		dragCoefficientPanel.setBackground(Color.white);
+		dragCoefficientPanel.add(dragC);
+		dragCoefficientPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), "Drag Coefficient"));
+
+
+		bottleRadius = new minimalSlider(minimalSlider.HORIZONTAL, 0, 10, 5);// divide by 100
+		
 		bottleRadius.setMajorTickSpacing(1);
 
 		bottleRadius.setMinorTickSpacing(1);
 
-		bottleRadius.setPaintTicks(true);
-
 		bottleRadius.setPaintLabels(true);
-
-		bottleRadius.setSnapToTicks(true);
-		group1.add(bottleRadius);
+		
 		bottleRadius.addChangeListener(this);
-		nozzleRadius = new JSlider(JSlider.HORIZONTAL, 1, 10, 1); // divide by
-																	// 100
-
+		
+		JPanel bottleRadiusPanel = new JPanel();
+		bottleRadiusPanel.setBackground(Color.white);
+		bottleRadiusPanel.add(bottleRadius);
+		bottleRadiusPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), "Bottle Radius"));
+		
+		
+		nozzleRadius = new minimalSlider (minimalSlider.HORIZONTAL, 1, 10, 1); // divide by
+																			  // 100
 		nozzleRadius.setMajorTickSpacing(1);
 
 		nozzleRadius.setMinorTickSpacing(1);
 
-		nozzleRadius.setPaintTicks(true);
-
 		nozzleRadius.setPaintLabels(true);
 
-		nozzleRadius.setSnapToTicks(true);
-
 		nozzleRadius.addChangeListener(this);
-
-		group1.add(nozzleRadius);
 		
+		JPanel nozzleRadiusPanel = new JPanel();
+		nozzleRadiusPanel.setBackground(Color.white);
+		nozzleRadiusPanel.add(nozzleRadius);
+		nozzleRadiusPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), "Nozzle Radius"));
 		
-
+		GroupLayout sliderLayout = new GroupLayout(sliders);
 		
-
+		sliders.setLayout(sliderLayout);
 		
+		sliderLayout.setAutoCreateContainerGaps(true);
+		sliderLayout.setAutoCreateGaps(true);
+		
+		GroupLayout.SequentialGroup horizontalLayout = sliderLayout.createSequentialGroup();
+		GroupLayout.SequentialGroup verticalLayout = sliderLayout.createSequentialGroup();
+		
+		horizontalLayout.addGroup(sliderLayout.createParallelGroup(Alignment.TRAILING)
+				.addComponent(rocketMassPanel)
+				.addComponent(airPressurePanel)
+				.addComponent(bottleRadiusPanel)
+				.addComponent(nozzleRadiusPanel));
 
-		// group2.add(fourthButton);
-		// group2.add(fifthButton);
-		// group2.add(sixthButton);
-		add(group1);
+		horizontalLayout.addGroup(sliderLayout.createParallelGroup()
+				.addComponent(waterMassPanel)
+				.addComponent(dragCoefficientPanel)
+				.addComponent(bottleVolumePanel));
 
+		sliderLayout.setHorizontalGroup(horizontalLayout);
+		
+		verticalLayout.addGroup(sliderLayout.createParallelGroup(Alignment.BASELINE)
+				.addComponent(rocketMassPanel).addComponent(waterMassPanel));
+		
+		verticalLayout.addGroup(sliderLayout.createParallelGroup()
+				.addComponent(airPressurePanel).addComponent(dragCoefficientPanel));
+		
+		verticalLayout.addGroup(sliderLayout.createParallelGroup()
+				.addComponent(bottleVolumePanel).addComponent(bottleRadiusPanel));
+
+		verticalLayout.addGroup(sliderLayout.createParallelGroup(Alignment.CENTER)
+				.addComponent(nozzleRadiusPanel));
+		
+		sliderLayout.setVerticalGroup(verticalLayout);
+		sliders.setBackground(Color.white);
+		setBackground(Color.white);
+		tabbedPane.add("sliders",sliders);
+		JPanel testPanel = new JPanel();
+		testPanel.setBackground(Color.white);
+		tabbedPane.add("exact values", testPanel);
+		tabbedPane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		add(tabbedPane);
 	}
-
-	private JPanel initBack(JPanel pane)
+	
+	public static void main(String[] args) 
 	{
-		BufferedImage image2 = null;
-		try {
-			image2 = ImageIO
-					.read(new File("background.jpeg"));
-		} catch (IOException e) {
-		
-			e.printStackTrace();
-		}
-		int type = image2.getType() == 0? BufferedImage.TYPE_INT_ARGB : image2.getType();
-		image2 = resizeImage(image2, type, 500, 500);
-		final BufferedImage image = image2;
-		
-		pane = new JPanel() {
-			@Override
-			protected void paintComponent(Graphics g) {
-				super.paintComponent(g);
-				g.drawImage(image, 0, 0, null);
-			}
-			
-			
-		};
-		return pane;
-	}
-	public static void main(String[] args) {
-
 		JFrame frame = new JFrame();
+		frame.setLayout(new BorderLayout());
 		frame.setTitle("Button Panel Example");
-		frame.setSize(600, 600);
+		frame.setSize(new Dimension(600,600));
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(new SliderPanel());
+		frame.add(new SliderPanel(), BorderLayout.PAGE_START);
+		frame.setBackground(Color.WHITE);
 		frame.setVisible(true);
-
 	}
 
-	private static BufferedImage resizeImage(BufferedImage originalImage, int type, int iw, int ih){
-		BufferedImage resizedImage = new BufferedImage(iw, ih, type);
-		Graphics2D g = resizedImage.createGraphics();
-		g.drawImage(originalImage, 0, 0, iw, ih, null);
-		g.dispose();
-	 
-		return resizedImage;
-	    }
 	@Override
 	public void stateChanged(ChangeEvent arg0) {
 		// TODO Auto-generated method stub
@@ -211,7 +245,7 @@ public class SliderPanel extends JPanel implements ChangeListener {
 		System.out.println("massWater: " + massWater.getValue() / (100 + 0.0));
 		System.out.println("volumeBottle: " + volumeBottle.getValue()
 				/ (1000 + 0.0));
-		System.out.println("airPressure: " + airPressure.getValue());
+		System.out.println("airPressure: " + airPressure.getValue()*10000);
 		System.out.println("dragC: " + dragC.getValue());
 		System.out.println("bottleRadius: " + bottleRadius.getValue()
 				/ (100 + 0.0));
