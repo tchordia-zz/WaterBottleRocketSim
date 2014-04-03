0192dc5f430c252b90f4f5e09c817e53ba6c8bf6
Cameron-Yang <cameronyang1@gmail.com>
2014-04-02 20:45:03 -0700

    hold up

--- RocketProjectTest/src/src/GUIpack/ScrollingScreenTest.java
+++ RocketProjectTest/src/src/GUIpack/ScrollingScreenTest.java
@@ -1,36 +1,11 @@
 package src.GUIpack;
 
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
-public class ScrollingScreenTest extends JPanel
+public class ScrollingScreenTest
 {
 	
-	public ScrollingScreenTest()
-	{
-		super();
-		setVisible(true);
-	}
-	
-	
-	@Override
-	public void paint(Graphics gr)
-	{
-		super.paint(gr);
-		gr.setColor(Color.blue);
-		gr.fillOval(getWidth()/2-10, getHeight()/2-10, 10, 10);
-	}
-	
-	
-	public static void main(String[] args)
-	{
-		ScrollingScreenTest screen = new ScrollingScreenTest();
-		JFrame frame = new JFrame();
-		frame.add(screen);
-		frame.setVisible(true);
-		frame.setSize(600,600);
-	}
 }
---
+++ RocketProjectTest/src/src/GUIpack/TrueCoordinates.java
@@ -0,0 +1,25 @@
+package src.GUIpack;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class TrueCoordinates extends AnimationBase implements ActionListener
+{
+	
+	int xOrigin;
+	int yOrigin;
+	
+	public TrueCoordinates()
+	{
+		super();
+		xOrigin=0;
+		yOrigin=0;
+	}
+	
+	public void moveCamera (int xIncrease, int yIncrease)
+	{
+		
+	}
+}
