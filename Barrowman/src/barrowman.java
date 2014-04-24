412e1e8ad590ad0a9ff4c79aff61f1dc776ad151
Sahil Sancheti <mockeagle@gmail.com>
2014-04-23 23:11:23 -0700

    Barrowman Eqns Complete- Cam tell me if they work properly
    Cameron tell me if they work they seem to produce ok numbers...

---
+++ Barrowman/src/barrowman.java
@@ -0,0 +1,112 @@
+/**
+ * barrowman eqns: calcs center of pressure / volume
+ * @author Sahil
+ *
+ */
+public class barrowman {
+	double ln;
+	double d;
+	double df;
+	double dr;
+	double lt;
+	double xp;
+	double cr;
+	double ct;
+	double s;
+	double lf;
+	double r;
+	double xr;
+	double xb;
+	double n;
+	double x; // distance of center of pressure from nose tip
+	double cnn = 2; // hardcoded for cone
+	double cnt;
+	double cnf;
+	double xn;
+	double xt;
+	double xf;
+	double volume;
+	/**
+	 *  cnn is hardcoded btw LOL
+	 * @param ln length of nose
+	 * @param d diameter at base of nose
+	 * @param df diameter at front of transition
+	 * @param dr diameter at rear of transition
+	 * @param lt length of transition
+	 * @param xp distance from tip of nose to front of transition
+	 * @param cr fin root chord
+	 * @param ct fin tip chord
+	 * @param s fin semispan
+	 * @param lf length of fin mid-chord line
+	 * @param r radius of body at aft end
+	 * @param xr distance between fin root leading edge and fin tip leading edge parallel to body
+	 * @param xb distance from nose tip to fin root chord leading edge
+	 * @param n number of fins
+	 */
+	public barrowman(double ln, double d, double df, double dr, double lt,
+			double xp, double cr, double ct, double s, double lf, double r,
+			double xr, double xb, double n) {
+		this.ln = ln;
+		xn = ln * .666;// this is intended for cone
+		this.d = d;
+		this.df = df;
+		this.dr = dr;
+		this.lt = lt;
+		this.xp = xp;
+		this.cr = cr;
+		this.ct = ct;
+		this.s = s;
+		this.lf = lf;
+		this.r = r;
+		this.xr = xr;
+		this.xb = xb;
+		this.n = n;
+		
+		
+		
+	}
+	
+	private double calcCNT() {
+		cnt = 2 * (Math.pow((dr / d),2) - Math.pow(df / d,2));
+		return cnt;
+	}
+
+	private double calcXT() {
+		xt = xp + lt / 3 * (1 + (1 - df / dr) / (1 - Math.pow(df / dr,2)));
+		return xt;
+	}
+
+	private double calcCNF() {
+		cnf = (1 + r / (s + r))	* (4 * n * Math.pow(s / d,2) ) / (1 + Math.sqrt(1 + Math.pow(2*lf/(cr+ct), 2)));
+		return cnf;
+	}
+
+	private double calcCF() {
+		xf = xb + xr / 3 * (cr + 2 * ct) / (cr + ct) + 1 / 6 * ((cr + ct) - (cr * ct / (cr + ct)));
+		return xf;
+	}
+	/**
+	 * call this method to get center of pressure 
+	 * @return x (distance from nosetip)
+	 */
+	public double centerOfPressure() {
+		calcCNT();
+		calcXT();
+		calcCNF();
+		calcCF();
+		x = (cnn * xn + cnt * xt + cnf * xf) / (cnn + cnt + cnf);
+		return x;
+	}
+	/**
+	 * call this method to get volume of rocket (includes the entire thing from top cylinder, transition, and bottom body
+	 * @return volume
+	 */
+	public double volume()
+	{
+		double v1=Math.PI*d/2*d/2*(xp-ln);
+		double v2=Math.PI*lt/3*(df/2*df/2+df/2*dr/2+dr/2*dr/2);
+		double v3=Math.PI*dr*dr*(xb-xp-lt+cr);// includes all the way to bottom of nozzle
+		volume = v1+v2+v3;
+		return volume;
+	}
+}
---
+++ Barrowman/src/runner.java
@@ -0,0 +1,11 @@
+
+public class runner {
+
+	public static void main(String[] args) {
+		// lol u can plug in legit numbers here or vaiables and then call methods like so to make it work
+		barrowman b = new barrowman(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+		System.out.println(b.centerOfPressure());
+		System.out.println(b.volume());
+	}
+
+}
