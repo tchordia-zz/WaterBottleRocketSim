47ba4b321026cd08a4bd6506b64640201492b21a
Sahil Sancheti <mockeagle@gmail.com>
2014-04-25 17:10:21 -0700

    added centerof mass to barrowman

--- Barrowman/src/runner.java
+++ Barrowman/src/runner.java
@@ -3,7 +3,7 @@ public class runner {
 
 	public static void main(String[] args) {
 		// lol u can plug in legit numbers here or vaiables and then call methods like so to make it work
-		barrowman b = new barrowman(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+		barrowman b = new barrowman(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,barrowman.cone);
 		System.out.println(b.centerOfPressure());
 		System.out.println(b.volume());
 	}
