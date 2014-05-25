ef3686cf9223119334e4fe779d9fff3725d7ca3a
Tanmay Chordia <tchordia@gmail.com>
2014-05-25 14:47:40 -0700

    html support

---
+++ BottleRocketFX/src/src/GUIpack/FullRocket.java
@@ -0,0 +1,46 @@
+package src.GUIpack;
+
+import java.io.Serializable;
+
+import javafx.scene.Group;
+
+public class FullRocket implements Serializable {
+CreateRocket g = null;
+AngularLaunch a = null;
+ProjectileBall b = null;
+
+public FullRocket()
+{
+}
+public FullRocket(CreateRocket r, ProjectileBall p, AngularLaunch a)
+{
+	b = p;
+	g = r;
+}
+public void update( CreateRocket r)
+{
+	g = r;
+}
+public void update( AngularLaunch r)
+{
+	a = r;
+}
+public void update( ProjectileBall r)
+{
+	b = r;
+}
+public CreateRocket getCreateRocket()
+{
+	return g;
+}
+
+public AngularLaunch getAngularLaunch()
+{
+	return a;
+}
+
+public ProjectileBall getProjectileBall()
+{
+	return b;
+}
+}
