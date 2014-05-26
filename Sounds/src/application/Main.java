8564d757729ac55d6fc58eadfee7b34d9b23a8b9
Sahil Sancheti <mockeagle@gmail.com>
2014-05-25 22:29:10 -0700

    Fixed sound class
    now we just need to copy paste these two lines whereever theres a sound
    and add files

--- Sounds/src/application/Main.java
+++ Sounds/src/application/Main.java
@@ -1,18 +1,15 @@
 package application;
 
-import java.awt.Toolkit;
+import java.io.File;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private  final AudioClip ALERT_AUDIOCLIP = new AudioClip("http://ragejunkie.com/wp-content/uploads/2014/01/DVBBS-Borgeous-Tsunami-Jay-Cosmic-Remix.mp3");
+	
 
     public static void main(String[] args) {
         launch(args);
@@ -20,25 +17,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        VBox root = new VBox();
-        Scene scene = new Scene(root, 300, 250);
-
-        Button btn = new Button();
-        btn.setText("Play (My Sound)");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                ALERT_AUDIOCLIP.play();
-            }
-        });
-
-       
-
-        root.getChildren().add(btn);
-       
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    	// these two lines play the musics
+    	MediaPlayer mediaPlayer = new MediaPlayer(new Media(new File("tsunami.mp3").toURI().toString()));
+    	mediaPlayer.play();
+        
     }
 }
